bd69ac102f15f78ad5d8d4dd2f5f5cef97fa53dd
Frank Dininno <mac6@129mac18.valleychristian.net>
2017-02-02 10:58:40 -0800

    commit for alert

--- webapp.js
+++ webapp.js
@@ -1,6 +1,18 @@
 // JavaScript Document
 
-/* document.getElementById("spaceship").addEventListener("click", function(){
-   alert("You gained 3 items!");
-});
-*/
+//$(document).ready(function() { document.getElementById("spaceship").addEventListener("click", function()alert("You gained 3 items!"))};
+function spaceship(room) {
+	switch(room) {
+		case "galley":
+			alert("this is the galley");
+			break;
+		case "cockpit":
+			alert("this is the cockpit");
+			break;
+			
+		case "engine":
+			alert("this is the engine room");
+			break;
+	}
+
+}
